a199dda23171bcfe3dfae6e0846369173d44316c
Raja Babu <contacrajababu@gmail.com>
2019-09-23 09:24:30 +0530

    Blog Function

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pubdate = models.DateTimeField()
+    body = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pubdate_custoize(self):
+        return self.pubdate.strftime('%b %e %y')
+
+
+
+
--- portfoliio/settings.py
+++ portfoliio/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'portfoliio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER':'postgres',
+        'PASSWORD':'rajababu',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
 
@@ -113,8 +119,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'portfoliio/static/')
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
